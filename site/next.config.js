8d452ce350dd7c759d77202a797e9af8e6d08c9c
Niklas Wonnebauer <wonnebauer.salkinxd@gmail.com>
2025-08-28 16:09:38 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://hobedere.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry-hobedere/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
